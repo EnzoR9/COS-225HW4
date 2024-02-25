c480f5e12c7df5ba8629ee058c4813ebe8856291
EnzoR9 <enzorussillo44@gmail.com>
2024-02-25 15:41:55 -0500

    start

--- Book.java
+++ Book.java
@@ -1,4 +1,18 @@
 public class Book
 {
+    public String title; 
+    public String genre;
+    
+    public Book(String t, String g)
+    {
+        this.title = t;
+        this.genre = g;
+    }
+    
+    public String toString()
+    {
+        return title;
+    }
+
     
 }
--- BookShelf.java
+++ BookShelf.java
@@ -1,4 +1,55 @@
+import java.util.ArrayList;
+
 public class BookShelf
 {
-    
+    public char firstLetter;
+    public ArrayList<Book> books;
+
+    public BookShelf(char shelfTitle)
+    {
+        this.books = new ArrayList<>();
+        this.firstLetter = shelfTitle;
+    }
+
+    public char getLetter()
+    {
+        return firstLetter;
+    }
+
+    public void addBook(Book book)
+    {
+        if(book.title.charAt(0) == firstLetter && books.size() < 8 )
+        {
+            books.add(book);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    public void remBook(Book book)
+    {
+        books.remove(book);
+    }
+
+    @Override
+    public String toString()
+    {
+        if (books.isEmpty())
+        {
+            return "-";
+        }
+        else 
+        {
+            String output = "";
+            for (Book book : books )
+            {
+                output += book.toString() + "   ";
+            }
+            return output.trim();
+        }
+    }
+
+   
 }
